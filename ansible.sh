b5cf5358949382ede019528f3b9e71289d1e9894
Nicolas Delsaux <nicolas.delsaux@etik.com>
2026-07-04 21:01:39 +0200

    Feat/add kavita (#22)
    * I don't need no sudo (neither reading values from keepass, since I have an ansible plugin for that)
    
    * I can haz a wordpress backup
    
    * Small upgrades
    
    * Maybe I can haaz some trala ? but it doesn't work yet)
    
    * Here is a nice way to have a nice homepage
    
    * Separated config file from ansible task
    
    * And now, kavita works!

--- ansible.sh
+++ ansible.sh
@@ -9,21 +9,10 @@ read -p "Quel est le mot de passe de '$keepass'?: " password
 stty echo
 printf '\n'
 
-
-DEFAULT_SYSTEM_USER="nicolas-delsaux"
-read -p  "Quel est l'utilisateur Linux? [$DEFAULT_SYSTEM_USER]" systemUser
-systemUser="${systemUser:-$defaultKeepass}"
-
-# Now read values from keepassxc database
-# keepassxc command-line is obviously slightly different
-
-qnapPassword=$(echo "$password" | keepassxc-cli show $keepass "QNAP" --show-protected --quiet --attributes "password")
-raspbianPassword=$(echo "$password" | keepassxc-cli show $keepass "Minidell" --show-protected --quiet --attributes "password")
-
 currentFolder=${PWD}
 # Finally start the docker image!
 # See https://stackoverflow.com/a/36648428 for the ssh socket madness
-docker="sudo docker run --rm --name ansible-for-server --hostname ansible-for-server -t -i -e ANSIBLE_KEEPASS_PSW=\"$password\" --mount type=bind,source=$keepass,target=/keepass.kdbx --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env RASPBIAN_PASSWORD=\"$raspbianPassword\" --env SSH_AUTH_SOCK=/ssh-agent -v $currentFolder/ansible:/ansible:ro willhallonline/ansible:2.18-ubuntu-24.04 /bin/bash"
+docker="docker run --replace --rm --name ansible-for-server --hostname ansible-for-server -t -i -e ANSIBLE_KEEPASS_PSW=\"$password\" --mount type=bind,source=$keepass,target=/keepass.kdbx --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env RASPBIAN_PASSWORD=\"$raspbianPassword\" --env SSH_AUTH_SOCK=/ssh-agent -v $currentFolder/ansible:/ansible:ro willhallonline/ansible:2.18-ubuntu-24.04 /bin/bash"
 
 echo $docker
 
